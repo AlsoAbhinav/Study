c79a66e913b43fb6df01772e63a087067e0864f6
Abhinav Sharma <aeroensharma@gmail.com>
2025-03-17 10:53:19 +0545

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import StudyPlanner from './components/StudyPlanner';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Study Planner</h1>
+      <StudyPlanner />
+    </div>
+  );
+};
+
+export default App;
